ff29fe6bdd44e655cb50570e92ffe75f4e1d203d
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2024-04-16 09:38:35 +0000

    Added CAInput Datasource
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- Analysis/getMdsData.py
+++ Analysis/getMdsData.py
@@ -6,20 +6,11 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from MDSplus import Tree
+import argparse
+
 
 mdsTreeName = 'rtappisttok'
 
-if len(sys.argv) > 1:
-    mdsPulseNumber = int(sys.argv[1])
-else:
-    mdsPulseNumber = 1
-    #treename = ''
-
-try:
-    tree = Tree(mdsTreeName, mdsPulseNumber)
-except:
-    print(f'Failed opening {mdsTreeName} for pulse number {mdsPulseNumber:d}')
-    exit()
 """
 Available Channels
 
@@ -30,6 +21,7 @@ ATCAIOP1.ADC3RAW
 ATCAIOP1.ADC4RAW
 ATCAIOP1.ADC5RAW
 ATCAIOP1.ADC6RAW
+#for i in range(args.irange[0], args.irange[1]):
 ATCAIOP1.ADC7RAW
 
 ATCAIOP1.ADC0INT
@@ -38,6 +30,23 @@ ATCAIOP1.ADC1INT
 """
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'Script to support the QA activities')
+
+    #parser.add_argument('-l','--list', nargs='+', help='<Required> Set flag', required=True)
+    #parser.add_argument('-l','--list', nargs='+')
+    parser.add_argument('-c', '--crange', nargs='+',type=int, help='Channel plots (1 12)',default=[1, 8])
+    parser.add_argument('-i', '--irange', nargs='+',type=int,default=[0, 4])
+#parser.add_argument('pulse','-', nargs='+', help='<Required> Set flag', required=True)
+    parser.add_argument('-s', '--shot', type=int, help='Mds+ pulse Number ([1, ...])', default=100)
+
+    args = parser.parse_args()
+    mdsPulseNumber = args.shot
+    try:
+        #tree = Tree(mdsTreeName, args.pulse)
+        tree = Tree(mdsTreeName, mdsPulseNumber)
+    except:
+        print(f'Failed opening {mdsTreeName} for pulse number {mdsPulseNumber:d}')
+        exit()
     mdsNode = tree.getNode("ATCAIOP1.ADC0RAW")
     mdsNode = tree.getNode("ATCAIOP1.ADC1INT")
     dataAdc = mdsNode.getData().data()
--- Analysis/pyqtPlotMds.py
+++ Analysis/pyqtPlotMds.py
@@ -1,10 +1,9 @@
+#!/usr/bin/env python3
 """
-This example demonstrates many of the 2D plotting capabilities
-in pyqtgraph. All of the plots may be panned/scaled by dragging with 
+This script plots the MARTe2 ATCAIop samples stored in MDSplus
+All of the plots may be panned/scaled by dragging with 
 the left/right mouse buttons. Right click on any plot to show a context menu.
 """
-
-#import sys
 import numpy as np
 
 import pyqtgraph as pg
@@ -24,8 +23,8 @@ parser = argparse.ArgumentParser(description = 'Script to support the QA activit
 
     #parser.add_argument('-l','--list', nargs='+', help='<Required> Set flag', required=True)
     #parser.add_argument('-l','--list', nargs='+')
-parser.add_argument('-c', '--crange', nargs='+',type=int, help='Channel plots (1 12)',default=[1, 8])
-parser.add_argument('-i', '--irange', nargs='+',type=int,default=[1, 8])
+parser.add_argument('-c', '--crange', nargs='+',type=int, help='Channel plots (1 12)',default=[1, 12])
+parser.add_argument('-i', '--irange', nargs='+',type=int,default=[1, 12])
 #parser.add_argument('pulse','-', nargs='+', help='<Required> Set flag', required=True)
 parser.add_argument('-s', '--shot', type=int, help='Mds+ pulse Number ([1, ...])', default=100)
 parser.add_argument('-m', '--maxsamples', type=int, help='Max samples to plot', default=50000)
@@ -33,12 +32,6 @@ parser.add_argument('-z', '--zero', action='store_true',help='Zero integral Line
 
 args = parser.parse_args()
 mdsPulseNumber = args.shot
-
-#if len(sys.argv) > 1:
-#    mdsPulseNumber = int(sys.argv[1])
-#else:
-#    mdsPulseNumber = 1
-#    #treename = ''
 
 mdsTreeName = 'rtappisttok'
 
@@ -55,11 +48,11 @@ win.setWindowTitle('pyqtgraph example: Plotting')
 # Enable antialiasing for prettier plots
 pg.setConfigOptions(antialias=True)
 
-p1 = win.addPlot(title="ATCA ADC raw decim")
+p1 = win.addPlot(title="ATCA-IOP ADC raw decimated")
 # add plt.addLegend() BEFORE you create the curves.
-mdsNode = tree.getNode("ATCAIOP1.ADC0RAW")
-dataAdc = mdsNode.getData().data()
-timeData = mdsNode.getDimensionAt(0).data()
+#mdsNode = tree.getNode("ATCAIOP1.ADC0RAW")
+#dataAdc = mdsNode.getData().data()
+#timeData = mdsNode.getDimensionAt(0).data()
 p1.addLegend()
 start = args.crange[0] -1; stop = args.crange[1] - 1
 #for i in range(args.crange[0], args.crange[1]):
@@ -74,21 +67,24 @@ for i in range(start, stop):
 #p1.setLabel('bottom', "Y Axis", units='s')
 
 win.nextRow()
-p4 = win.addPlot(title="ATCA Integral Channels")
+p4 = win.addPlot(title="Channel Integrals")
 p4.addLegend()
 #for i in range(8,12):
 start = args.irange[0] -1; stop = args.irange[1] - 1
 for i in range(start, stop):
-#for i in range(args.irange[0], args.irange[1]):
+    #mdsNode = tree.getNode(f"ATCAIOP1.ADC8INT")
     mdsNode = tree.getNode(f"ATCAIOP1.ADC{i}INT")
-    dataAdcInt = mdsNode.getData().data()
-    timeData = mdsNode.getDimensionAt(0).data()
-    y = dataAdcInt[ :args.maxsamples, 0]
-    if(args.zero):
-        y = y - dataAdcInt[0, 0]
-    #x = DECIM_RATE * np.arange(len(y))
-    x = DECIM_RATE * np.arange(len(y)) / 2.0e6
-    p4.plot(x,y, pen=pg.mkPen(i, width=2), name=f"Ch {i+1}")
+    try:
+        #print(f"ATCAIOP1.ADC{i:d}INT")
+        dataAdcInt = mdsNode.getData().data() / 2.0e6 #  LSB * sec
+        timeData = mdsNode.getDimensionAt(0).data()
+        y = dataAdcInt[ :args.maxsamples, 0]
+        if(args.zero):
+            y = y - dataAdcInt[0, 0]
+        x = DECIM_RATE * np.arange(len(y)) / 2.0e6 # in sec
+        p4.plot(x,y, pen=pg.mkPen(i, width=2), name=f"Ch {i+1}")
+    except:
+        print(f"No data ATCAIOP1.ADC{i:d}INT")
 
 p4.setLabel('bottom', "Time", units='s')
 
