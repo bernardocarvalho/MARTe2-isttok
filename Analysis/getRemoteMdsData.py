2c04675200192ab72fa3c12501b36c4a187869b7
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2024-10-14 23:38:51 +0100

    Added Thin Client remote access
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

---
+++ Analysis/getRemoteMdsData.py
@@ -0,0 +1,9 @@
+""" 
+Thin client configuration
+https://www.mdsplus.org/index.php?title=Documentation:Tutorial:RemoteAccess&open=101601206494234739185&page=Documentation%2FTutorials%2FRemote+RemoteAccess
+"""
+from MDSplus import Connection
+conn = Connection('epics.ipfn.tecnico.ulisboa.pt')
+conn.openTree('rtappisttok', 131)
+result = conn.get("ATCAIOP1.ADC0RAW")
+result.data()
